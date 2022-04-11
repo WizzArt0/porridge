e853e421f23c876b85cb806752a28e344705325e
Alexei Ignatkevich <lexa23523@gmail.com>
2022-04-11 11:42:51 +0300

    add body with playlist and songs on player page, add control panel for songs

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         token: _token,
       });
 
-      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+      s.getPlaylist("37i9dQZF1EVHGWrwldPRtj").then((response) =>
         dispatch({
           type: "SET_DISCOVER_WEEKLY",
           discover_weekly: response,
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Sidebar from './../Sidebar/Sidebar'
+import Body from './../Body/Body'
+import Footer from './../Footer/Footer'
+import './Player.css'
 
 function Player({ spotify }) {
   return (
     <div className="player">
       <div className="player__body">
-        <Sidebar />
-        {/* <Body spotify={spotify} /> */}
+      <Sidebar />
+        <Body spotify={spotify} />
       </div>
-      {/* <Footer spotify={spotify} /> */}
+      <Footer spotify={spotify} />
     </div>
   );
 }
